8bd7604c51b0b131ee9c074ab27fb8cb48bf3680
FOUREST <jmfourest@gmail.com>
2023-01-18 14:40:15 +0100

    jeu

--- src/main/java/com/coding/puissance/module/Grid.java
+++ src/main/java/com/coding/puissance/module/Grid.java
@@ -38,4 +38,13 @@ public class Grid {
         System.out.println("-----------------------------");
     }
 
+    public static void dropPiece(String[][] matrix, int colonne, String player){
+        int line = 5;
+
+        while (!matrix[line][colonne].equals(".")) {
+            line--;
+        }
+        matrix[line][colonne] = player;
+    }
+
 }
