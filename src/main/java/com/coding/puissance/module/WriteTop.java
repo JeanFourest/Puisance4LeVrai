8676f407c2f23779d454d76b95144a473c2813a0
fabien <fabien.arnal@edu.esiee-it.fr>
2023-01-19 14:18:23 +0100

    push

--- src/main/java/com/coding/puissance/module/WriteTop.java
+++ src/main/java/com/coding/puissance/module/WriteTop.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 
 public class WriteTop{
 
+    //public void writetop(){
     public void writetop(int turn, String name){
 
         //String player = "jordan";
@@ -14,14 +15,22 @@ public class WriteTop{
 
 
         try{
+            //créer un fichier TOP.txt si inexistant
             File file = new File("TOP.txt");
 
+            //si le fichier existe alors...
             if (file.exists()){
 
+                //vérifie les commodités pour l'écriture des fichiers caractères
                 FileWriter writer = new FileWriter(file, true);
+
+                //Écrit du texte dans un flux de sortie de caractères, en mettant les caractères en mémoire tampon
+                // afin de permettre l'écriture efficace de caractères uniques, de tableaux et de chaînes.
                 BufferedWriter bw = new BufferedWriter(writer);
 
+                //ecrit le gagnant dans le fichier sous la forme suivante :
                 bw.write(name + " : " + turn + ";");
+                //créé une nouvelle ligne
                 bw.newLine();
                 bw.close();
 
