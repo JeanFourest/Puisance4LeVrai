7d7bd618f2b694f31ad2736d431c15079c93f1b3
fabien <fabien.arnal@edu.esiee-it.fr>
2023-01-19 13:55:39 +0100

    update writetop

--- src/main/java/org/example/top/WriteTop.java
+++ src/main/java/org/example/top/WriteTop.java
@@ -10,19 +10,27 @@ public class WriteTop{
     public void writetop(){
     //public void writetop(turn, name){
 
-        String player = "jordan";
-        int score = 16;
+        //String player = "jordan";
+        //int score = 16;
 
 
         try{
+            //créer un fichier TOP.txt si inexistant
             File file = new File("TOP.txt");
 
+            //si le fichier existe alors...
             if (file.exists()){
 
+                //vérifie les commodités pour l'écriture des fichiers caractères
                 FileWriter writer = new FileWriter(file, true);
+
+                //Écrit du texte dans un flux de sortie de caractères, en mettant les caractères en mémoire tampon
+                // afin de permettre l'écriture efficace de caractères uniques, de tableaux et de chaînes.
                 BufferedWriter bw = new BufferedWriter(writer);
 
-                bw.write(player + " : " + score + ";");
+                //ecrit le gagnant dans le fichier sous la forme suivante :
+                bw.write(name + " : " + turn + ";");
+                //créé une nouvelle ligne
                 bw.newLine();
                 bw.close();
 
